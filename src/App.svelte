84f16213770a8d17b70ff4aac04cad920c78ae38
willdebras <wtb3na@virginia.edu>
2021-07-13 16:19:22 -0500

    Removing useless function

--- src/App.svelte
+++ src/App.svelte
@@ -5,15 +5,6 @@ let inputPower = "";
 let squaredJS = "";
 let squaredR = "";
 
-async function searchForGif(e) {
-  try {
-    const returnValue = await fetch(`/giphy?term=${searchTerm}`);
-    const response = await returnValue.json();
-    gifs = response.data;
-  } catch (error) {
-    console.error(error);
-  }
-}
 
 async function handleSubmit(e) {
 		//e.preventDefault();
